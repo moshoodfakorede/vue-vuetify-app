e27ee362bbf11b30765c4f87456a27fbcc485c98
moshoodfakorede <moshood@armpension.com>
2021-01-05 01:33:21 +0100

    header styles, image and title

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify App</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero perspiciatis ut harum totam repudiandae, asperiores laboriosam sint officiis sapiente nam praesentium. Enim sunt, amet corrupti voluptas deserunt asperiores ad!
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero perspiciatis ut harum totam repudiandae, asperiores laboriosam sint officiis sapiente nam praesentium. Enim sunt, amet corrupti voluptas deserunt asperiores ad!
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero perspiciatis ut harum totam repudiandae, asperiores laboriosam sint officiis sapiente nam praesentium. Enim sunt, amet corrupti voluptas deserunt asperiores ad!
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vero perspiciatis ut harum totam repudiandae, asperiores laboriosam sint officiis sapiente nam praesentium. Enim sunt, amet corrupti voluptas deserunt asperiores ad!
+    </p>
   </div>
 </template>
